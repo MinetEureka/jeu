8cab701832a699a0c4e0c005df3b4ffcf19c0eb9
Minet <dmx87816@kwansei.ac.jp>
2026-03-03 17:20:38 +0900

    数字全種コンプリート

---
+++ L5/js/r4.js
@@ -0,0 +1,23 @@
+/* responses (外部化) */
+window.responses = Object.freeze({
+"01a": "je chante",
+"01b": "je ne chante pas",
+"02a": "je danse",
+"02b": "je ne danse pas",
+"03a": "je suis",
+"03b": "je ne suis pas",
+"04a": "je suis",
+"04b": "je ne suis pas",
+"05a": "je suis",
+"05b": "je ne suis pas",
+"06a": "j'étudie",
+"06b": "je n'étudie pas",
+"07a": "j'habite",
+"07b": "je n'habite pas",
+"08a": "je mange",
+"08b": "je ne mange pas",
+"09a": "je parle",
+"09b": "je ne parle pas",
+"10a": "je voyage",
+"10b": "je ne voyage pas"
+});
