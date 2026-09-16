8896e3cbc5d9ee68be157d96457ab1f02c9a1506
Minet <dmx87816@kwansei.ac.jp>
2026-09-16 15:56:34 +0900

    F21-2-3バグfixed

--- resto-v/js/config.js
+++ resto-v/js/config.js
@@ -2,7 +2,7 @@
 window.gameConfig = Object.freeze({
   maxCards: null,       // nullならreponses.jsのmaxVerbeを使用。指定する場合は1〜50。
   rounds: 5,           // 出題数
-  answerSeconds: 7,     // 回答時間。5秒にしたい場合は 5 に変更。
+  answerSeconds: 10,     // 回答時間。5秒にしたい場合は 5 に変更。
 
   // 練習・終了後確認で使う正解音声。
   // 01=最初の区間、02=次の区間…。
